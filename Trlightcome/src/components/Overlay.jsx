2f7ba9e095bc800642b3240d79be8a9e527ba414
BrandMasala <smbrandmasala@gmail.com>
2025-06-09 10:31:13 +0530

    a

---
+++ Trlightcome/src/components/Overlay.jsx
@@ -0,0 +1,44 @@
+import { useProgress } from "@react-three/drei";
+import { usePlay } from "../contexts/Play";
+
+export const Overlay = () => {
+  const { progress } = useProgress();
+  const { play, end, setPlay, setEnd, hasScroll } = usePlay();
+  return (
+    <div
+      className={`overlay ${play ? "overlay--disable" : ""}
+    ${hasScroll ? "overlay--scrolled" : ""}`}
+    >
+      <div
+        className={`loader ${progress === 100 ? "loader--disappear" : ""}`}
+      />
+      {progress === 100 && (
+        <div className={`intro ${play ? "intro--disappear" : ""}`}>
+          <div className="logo-image" />
+          <p className="intro__scroll">Scroll to begin the journey</p>
+          <button
+            className="explore"
+            onClick={() => {
+              setPlay(true);
+            }}
+          >
+            Explore
+          </button>
+        </div>
+      )}
+      <div className={`outro ${end ? "outro--appear" : ""}`}>
+        <p className="outro__text">Wish you had a great journey with us...</p>
+      
+        <button
+          className="outro__button"
+          onClick={() => {
+            setEnd(false);
+            setPlay(true);
+          }}
+        >
+          Know about us more
+        </button>
+      </div>
+    </div>
+  );
+};
