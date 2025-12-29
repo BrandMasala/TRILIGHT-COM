352cebb094a7fd89d0adffea9ced514423b6e7ff
Kothakota Abhinav <smbrandmasala@gmail.com>
2025-12-29 17:47:08 +0530

    e

--- Trilightlp/src/data/gallery.js
+++
@@ -1,8 +0,0 @@
-export const galleryItems = [
-  { id: 'g1', name: 'Sky Mansion Interior', location: 'Business Bay', image: '/gallery/g1.svg', video: undefined },
-  { id: 'g2', name: 'Lobby Atrium', location: 'Downtown Dubai', image: '/gallery/g2.svg', video: undefined },
-  { id: 'g3', name: 'Penthouse Lounge', location: 'Jumeirah', image: '/gallery/g3.svg', video: undefined },
-  { id: 'g4', name: 'Facade Detail', location: 'Business Bay', image: '/gallery/g4.svg', video: undefined },
-  { id: 'g5', name: 'Pool Deck', location: 'Al Jaddaf', image: '/gallery/g5.svg', video: undefined },
-  { id: 'g6', name: 'Signature Suite', location: 'Business Bay', image: '/gallery/g6.svg', video: undefined },
-]
